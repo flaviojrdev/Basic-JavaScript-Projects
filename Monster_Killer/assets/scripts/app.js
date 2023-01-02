2660cf4e876820ad5ea80925c1c083b1f687eae4
FlavioJR-Dev <d-garm@outlook.com>
2023-01-01 20:33:02 -0400

    Added user prompt and minor refactoring

--- Monster_Killer/assets/scripts/app.js
+++ Monster_Killer/assets/scripts/app.js
@@ -3,7 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const ENTERED_VALUE = prompt('Maximum life for you and the monster', '100');
+
+let chosenMaxLife = parseInt(ENTERED_VALUE);
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
@@ -20,14 +22,12 @@ function endRound() {
   const initialPlayerHealth = currentPlayerHealth;
   const damagePlayer = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= damagePlayer;
-
   if (currentPlayerHealth <= 0 && hasBonusLife) {
     hasBonusLife = false;
     removeBonusLife();
     currentPlayerHealth = initialPlayerHealth;
     alert("You would be dead but the bonus life saved you!");
   }
-
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You won!");
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
@@ -35,7 +35,6 @@ function endRound() {
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert("You have a draw!");
   }
-
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     reset();
   }
@@ -43,16 +42,13 @@ function endRound() {
 
 function attackMonster(mode) {
   let maxDamage;
-
   if (mode === "ATTACK") {
     maxDamage = ATTACK_VALUE;
   } else if (mode === "STRONG_ATTACK") {
     maxDamage = STRONG_ATTACK_VALUE;
   }
-
   const damageMonster = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damageMonster;
-
   endRound();
 }
 
@@ -66,16 +62,14 @@ function strongAttackHandler() {
 
 function healPlayerHandler() {
   let healValue;
-
   if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
     alert("You can't heal to more than your max initial health.");
     healValue = chosenMaxLife - currentPlayerHealth;
   } else {
     healValue = HEAL_VALUE;
   }
-
-  increasePlayerHealth(HEAL_VALUE);
-  currentPlayerHealth += HEAL_VALUE;
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
   endRound();
 }
 
